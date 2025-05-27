5d95aa86d50fa284d5c89aa150b0d43a34d45eea
Brewster Malevich <bmalevich@rhg.com>
2025-05-27 15:47:49 -0700

    Add basic help, types, defaults to CLI

--- src/ssp_landwaterstorage/cli.py
+++ src/ssp_landwaterstorage/cli.py
@@ -17,22 +17,106 @@ import click
 
 
 @click.command()
-@click.option("--scenario", envvar="SSP_LANDWATERSTORAGE_SCENARIO")
-@click.option("--dotriangular", envvar="SSP_LANDWATERSTORAGE_DOTRIANGULAR")
-@click.option("--includepokherl", envvar="SSP_LANDWATERSTORAGE_INCLUDEPOKHERL")
-@click.option("--baseyear", envvar="SSP_LANDWATERSTORAGE_BASEYEAR")
-@click.option("--pyear-start", envvar="SSP_LANDWATERSTORAGE_PYEAR_START")
-@click.option("--pyear-end", envvar="SSP_LANDWATERSTORAGE_PYEAR_END")
-@click.option("--pyear-step", envvar="SSP_LANDWATERSTORAGE_PYEAR_STEP")
-@click.option("--nsamps", envvar="SSP_LANDWATERSTORAGE_NSAMPS")
-@click.option("--seed", envvar="SSP_LANDWATERSTORAGE_SEED")
-@click.option("--pipeline-id", envvar="SSP_LANDWATERSTORAGE_PIPELINE_ID")
-@click.option("--dcyear-start", envvar="SSP_LANDWATERSTORAGE_DCYEAR_START")
-@click.option("--dcyear-end", envvar="SSP_LANDWATERSTORAGE_DCYEAR_END")
-@click.option("--dcrate-lo", envvar="SSP_LANDWATERSTORAGE_DCRATE_LO")
-@click.option("--dcrate-hi", envvar="SSP_LANDWATERSTORAGE_DCRATE_HI")
-@click.option("--locationfile", envvar="SSP_LANDWATERSTORAGE_LOCATIONFILE")
-@click.option("--chunksize", envvar="SSP_LANDWATERSTORAGE_CHUNKSIZE")
+@click.option(
+    "--scenario",
+    envvar="SSP_LANDWATERSTORAGE_SCENARIO",
+    help="Use RCP or SSP scenario.",
+    default="rcp85",
+)
+@click.option(
+    "--dotriangular",
+    envvar="SSP_LANDWATERSTORAGE_DOTRIANGULAR",
+    help="Use triangular distribution for GWD.",
+    default=False,
+)
+@click.option(
+    "--includepokherl",
+    envvar="SSP_LANDWATERSTORAGE_INCLUDEPOKHERL",
+    help="Include Pokhrl data for GWD.",
+    default=False,
+)
+@click.option(
+    "--baseyear",
+    envvar="SSP_LANDWATERSTORAGE_BASEYEAR",
+    help="Base year to which projections are centered.",
+    default=2000,
+    type=click.IntRange(2000, 2010),
+)
+@click.option(
+    "--pyear-start",
+    envvar="SSP_LANDWATERSTORAGE_PYEAR_START",
+    help="Year for which projections start.",
+    default=2000,
+    type=click.IntRange(min=2000),
+)
+@click.option(
+    "--pyear-end",
+    envvar="SSP_LANDWATERSTORAGE_PYEAR_END",
+    help="Year for which projections end.",
+    default=2100,
+    type=click.IntRange(max=2300),
+)
+@click.option(
+    "--pyear-step",
+    envvar="SSP_LANDWATERSTORAGE_PYEAR_STEP",
+    help="Step size in years between start and end at which projections are produced",
+    default=10,
+    type=click.IntRange(min=1),
+)
+@click.option(
+    "--nsamps",
+    envvar="SSP_LANDWATERSTORAGE_NSAMPS",
+    help="Number of samples to generate.",
+    default=20000,
+)
+@click.option(
+    "--seed",
+    envvar="SSP_LANDWATERSTORAGE_SEED",
+    help="Seed value for random number generator.",
+    default=1234,
+)
+@click.option(
+    "--pipeline-id",
+    envvar="SSP_LANDWATERSTORAGE_PIPELINE_ID",
+    help="Unique identifier for this instance of the module.",
+    required=True,
+)
+@click.option(
+    "--dcyear-start",
+    envvar="SSP_LANDWATERSTORAGE_DCYEAR_START",
+    help="Year in which dam correction application is started.",
+    default=2020,
+)
+@click.option(
+    "--dcyear-end",
+    envvar="SSP_LANDWATERSTORAGE_DCYEAR_END",
+    help="Year in which dam correction application is ended.",
+    default=2040,
+)
+@click.option(
+    "--dcrate-lo",
+    envvar="SSP_LANDWATERSTORAGE_DCRATE_LO",
+    help="Lower bound of dam correction rate.",
+    default=0.0,
+)
+@click.option(
+    "--dcrate-hi",
+    envvar="SSP_LANDWATERSTORAGE_DCRATE_HI",
+    help="Upper bound of dam correction rate.",
+    default=0.0,
+)
+@click.option(
+    "--locationfile",
+    envvar="SSP_LANDWATERSTORAGE_LOCATIONFILE",
+    help="File container name, id, lat, and lon of points for localization.",
+    default="location.lst",
+)
+@click.option(
+    "--chunksize",
+    envvar="SSP_LANDWATERSTORAGE_CHUNKSIZE",
+    help="Number of locations to process at a time.",
+    default=50,
+)
 def main(
     scenario,
     dotriangular,
@@ -51,6 +135,9 @@ def main(
     locationfile,
     chunksize,
 ) -> None:
+    """
+    Project groundwater depletion and dam impoundment contributions to sea level. See IPCC AR6 WG1 9.6.3.2.6.
+    """
     click.echo("Hello from ssp-landwaterstorage!")
     ssp_preprocess_landwaterstorage(
         scenario,
